b54b55d7808249b558e1a6b8f3b9b6c97883e6af
unknown <souzasilasmarcos@gmail.com>
2023-12-14 22:04:14 -0300

    add env:dev

---
+++ env/dev/main.tf
@@ -0,0 +1,19 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 4.16"
+    }
+  }
+
+  required_version = ">= 1.2.0"
+}
+
+provider "aws" {
+  region  = "sa-east-1"
+}
+
+resource "aws_instance" "app_server" {
+  ami           = "ami-0b6c2d49148000cd5"
+  instance_type = "t2.micro"
+}
