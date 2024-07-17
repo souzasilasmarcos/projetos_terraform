ddb1b0a94ca1d4061d9082e624bad08e5e228000
souzasilasmarcos <souzasilasmarcos@gmail.com>
2024-07-16 21:36:02 -0300

    fix: remove ec2 env:dev

--- env/dev/output.tf
+++ env/dev/output.tf
@@ -1,10 +1,10 @@
-output "instance_id" {
-  description = "app_server"
-  value       = aws_instance.app_server.id
-}
+#output "instance_id" {
+#  description = "app_server"
+#  value       = aws_instance.app_server.id
+#}
 
-output "instance_public_ip" {
-  description = "app_server IP public"
-  value       = aws_instance.app_server.public_ip
-
-}
+#output "instance_public_ip" {
+#  description = "app_server IP public"
+#  value       = aws_instance.app_server.public_ip
+#
+#}
