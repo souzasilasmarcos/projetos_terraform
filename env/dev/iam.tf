95bde0edd8202833a96bab1fefd27db1b7754790
souzasilasmarcos <souzasilasmarcos@gmail.com>
2024-07-16 21:27:25 -0300

    fix: update iaac func:lambda env:dev

--- env/dev/iam.tf
+++ env/dev/iam.tf
@@ -1,20 +1,46 @@
 resource "aws_iam_role" "lambda_role" {
-  name = "${local.workload.env}-${local.workload.name}-role"
-  assume_role_policy = jsonencode({
-    Version = "2012-10-17"
-    Statement = [
-      {
-        Action = "sts:AssumeRole"
-        Effect = "Allow"
-        Principal = {
-          Service = "lambda.amazonaws.com"
-        }
-      },
-    ]
-  })
+  name               = "${local.workload.env}-${local.workload.name}-role"
+  assume_role_policy = <<EOF
+{
+ "Version": "2012-10-17",
+ "Statement": [
+   {
+     "Action": "sts:AssumeRole",
+     "Principal": {
+       "Service": "lambda.amazonaws.com"
+     },
+     "Effect": "Allow",
+     "Sid": ""
+   }
+ ]
+}
+EOF
 }
 
-resource "aws_iam_role_policy_attachment" "lambda_policy" {
+resource "aws_iam_policy" "iam_policy_for_lambda" {
+
+  name        = "${local.workload.env}-${local.workload.name}-policy"
+  path        = "/"
+  description = "AWS IAM Policy for managing aws lambda role"
+  policy      = <<EOF
+{
+ "Version": "2012-10-17",
+ "Statement": [
+   {
+     "Action": [
+       "logs:CreateLogGroup",
+       "logs:CreateLogStream",
+       "logs:PutLogEvents"
+     ],
+     "Resource": "arn:aws:logs:*:*:*",
+     "Effect": "Allow"
+   }
+ ]
+}
+EOF
+}
+
+resource "aws_iam_role_policy_attachment" "attach_iam_policy_to_iam_role" {
   role       = aws_iam_role.lambda_role.name
-  policy_arn = "arn:aws:iam::aws:policy/service-role/AWSLambdaBasicExecutionRole"
+  policy_arn = aws_iam_policy.iam_policy_for_lambda.arn
 }
--- env/dev/locals.tf
+++ env/dev/locals.tf
@@ -12,11 +12,12 @@ locals {
 
 
 resource "aws_lambda_function" "lambda_function" {
-  filename         = "${path.module}/lambda_function_payload.zip"
-  function_name    = "${local.workload.env}-${local.workload.name}-lambda"
-  role             = aws_iam_role.lambda_role.arn
-  handler          = "function.lambda_handler"  
-  runtime          = "python3.10"
+  filename      = "${path.module}/lambda_function_payload.zip"
+  function_name = "${local.workload.env}-${local.workload.name}-lambda"
+  role          = aws_iam_role.lambda_role.arn
+  handler       = "function.lambda_handler"
+  runtime       = "python3.10"
+  depends_on    = [aws_iam_role_policy_attachment.attach_iam_policy_to_iam_role]
 
   environment {
     variables = {
