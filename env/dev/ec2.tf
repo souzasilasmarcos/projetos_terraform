a850575312fc38b522d8040f95fd9cd0d363a8c2
souzasilasmarcos <souzasilasmarcos@gmail.com>
2024-07-16 21:35:01 -0300

    fix: remove ec2 env:dev

--- env/dev/ec2.tf
+++ env/dev/ec2.tf
@@ -1,4 +1,4 @@
-resource "aws_instance" "app_server" {
-  ami           = "ami-0b6c2d49148000cd5"
-  instance_type = "t2.nano"
-}
+#resource "aws_instance" "app_server" {
+#  ami           = "ami-0b6c2d49148000cd5"
+#  instance_type = "t2.nano"
+#}
